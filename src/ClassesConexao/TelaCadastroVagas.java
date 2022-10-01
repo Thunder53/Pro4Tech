91e283c91602247ed9a45e5c18120b5825de23a4
Vitoria Brancatti <vitoriabrancatti16@gmail.com>
2022-09-30 22:06:08 -0300

    TelaCadastroVagas

---
+++ src/ClassesConexao/TelaCadastroVagas.java
@@ -0,0 +1,183 @@
+package ClassesConexao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.BevelBorder;
+
+public class TelaCadastroVagas extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCadastroVagas frame = new TelaCadastroVagas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCadastroVagas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1920, 1080);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setForeground(new Color(255, 255, 255));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("D:\\Users\\Vitoria Brancatti\\Documents\\FATEC\\PRO4TEC\\PRO4TECH 22.jpg"));
+		lblNewLabel.setBounds(20, 11, 819, 140);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("VOCÊ ESTÁ NA PÁGINA DE CADASTRO DE VAGAS!\r\n");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(357, 151, 591, 52);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("INSIRA ABAIXO AS INFORMAÇÕES DA VAGA QUE DESEJA CADASTRAR:");
+		lblNewLabel_2.setToolTipText("");
+		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(357, 180, 629, 52);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("NOME VAGA:");
+		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_3.setBounds(468, 248, 111, 28);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("CARGA HORÁRIA: ");
+		lblNewLabel_4.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_4.setBounds(423, 287, 156, 28);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("MODELO DE TRABALHO:");
+		lblNewLabel_5.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_5.setBounds(365, 326, 214, 28);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("SALÁRIO:");
+		lblNewLabel_6.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_6.setBounds(499, 365, 80, 28);
+		contentPane.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("REQUISITOS:");
+		lblNewLabel_7.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_7.setBounds(464, 404, 115, 28);
+		contentPane.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("ESCOLARIDADE:");
+		lblNewLabel_8.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_8.setBounds(436, 448, 143, 28);
+		contentPane.add(lblNewLabel_8);
+		
+		JLabel lblNewLabel_9 = new JLabel("SOFT SKILLS DESEJADAS:");
+		lblNewLabel_9.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_9.setBounds(350, 487, 229, 28);
+		contentPane.add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("HARD SKILLS DESEJADAS:");
+		lblNewLabel_10.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_10.setBounds(349, 529, 230, 28);
+		contentPane.add(lblNewLabel_10);
+		
+		JLabel lblNewLabel_11 = new JLabel("RESPONSÁVEL:");
+		lblNewLabel_11.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_11.setBounds(442, 570, 137, 28);
+		contentPane.add(lblNewLabel_11);
+		
+		textField = new JTextField();
+		textField.setBounds(589, 250, 400, 28);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(589, 289, 400, 28);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(589, 328, 400, 28);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(589, 367, 400, 28);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(589, 406, 400, 28);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(589, 445, 400, 28);
+		contentPane.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(589, 489, 400, 28);
+		contentPane.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(589, 531, 400, 28);
+		contentPane.add(textField_7);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(589, 572, 400, 28);
+		contentPane.add(textField_8);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("SE ESTIVER TUDO CERTO, CLIQUE NO BOTÃO \"OK\"");
+		lblNewLabel_2_1.setBackground(Color.ORANGE);
+		lblNewLabel_2_1.setToolTipText("");
+		lblNewLabel_2_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_2_1.setBounds(357, 623, 460, 52);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnNewButton.setFont(new Font("Arial", Font.BOLD, 18));
+		btnNewButton.setBackground(Color.ORANGE);
+		btnNewButton.setBounds(830, 623, 156, 52);
+		btnNewButton.setForeground(Color.BLACK);
+		contentPane.add(btnNewButton);
+	}
+}
